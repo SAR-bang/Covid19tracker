9938781046556bb7be5d9c9d502a281b291c2647
SAR-bang <palpaliadarsha@gmail.com>
2020-06-05 15:48:42 +0545

    addded

--- app/src/main/java/com/example/finalproject/News.java
+++ app/src/main/java/com/example/finalproject/News.java
@@ -7,6 +7,9 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.MediaController;
 import android.widget.VideoView;
@@ -18,36 +21,6 @@ public class News extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news);
 
-        final VideoView videoView = findViewById(R.id.video_id);
-        Button play = findViewById(R.id.play_id);
-        Button pause = findViewById(R.id.pause_id);
-
-
-
-        MediaController mediaController = new MediaController(this);
-//        mediaController.setAnchorView(videoView);
-
-        Uri uri = Uri.parse("https://www.youtube.com/watch?v=7zzfdYShvQU");
-
-        // setting media controller
-        videoView.setMediaController(mediaController);
-        videoView.setVideoURI(uri);
-        videoView.requestFocus();
-        videoView.start();
-
-        play.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                videoView.start();
-            }
-        });
-
-        pause.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                videoView.pause();
-            }
-        });
 
     }
 }
